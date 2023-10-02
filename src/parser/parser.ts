772d5389a040bdeda075842d278e9440e2049be4
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-10-02 17:41:15 -0300

    Working on adding grammar interpretation

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,21 +1,49 @@
-import { Token } from "../types";
+import { Position, Range } from "vscode-languageserver";
+import { FunctionDefinition, Token, VariableDefinition } from "../types";
 import { Tokenizer } from "./tokenizer";
 
+/**
+ * Parses a string (text) into an AST.
+ */
 export class Parser {
-  private tokenizer: Tokenizer;
-  private tokens: Token[] = [];
 
-  constructor(text: string) {
-    this.tokenizer = new Tokenizer(text);
+  private tokens: Token[] = [];
+  private functionDefinitions: FunctionDefinition[];
+  private variableDefinitions: VariableDefinition[];
+
+  constructor(private text: string) {
+    this.functionDefinitions = [];
+    this.variableDefinitions = [];
     this.getAllTokens();
   }
 
   private getAllTokens(): void {
+    const tokenizer = new Tokenizer(this.text);
     let token: Token;
     do {
-      token = this.tokenizer.getNextToken();
+      token = tokenizer.getNextToken();
       this.tokens.push(token);
     } while (token.type !== "EOF");
+  }
+
+  /**
+   * Extracts the functions definitions from the tokens in the text.
+   * @returns {void} void
+   */
+  private createGrammar(): void {
+    for (let i = 0; i < this.tokens.length - 1; i++) {
+      const currentToken = this.tokens[i];
+      const nextToken = this.tokens[i + 1];
+
+      if (currentToken.type === "IDENTIFIER" && nextToken.type === "EQUALS") {
+        this.variableDefinitions.push({
+          name: currentToken.content,
+          content: "content",
+          position: Range.create(Position.create(0, 0), Position.create(0, 0))
+        });
+      }
+
+    }
   }
 
   /**
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,4 @@
+import { Range } from "vscode-languageserver";
 import { Symbols } from ".";
 
 export type TokenType = keyof typeof Symbols | "LITERAL" | "NUMBER" | "ILLEGAL" | "IDENTIFIER" | "STRING" | "KEYWORD";
@@ -5,4 +6,17 @@ export type TokenType = keyof typeof Symbols | "LITERAL" | "NUMBER" | "ILLEGAL" 
 export type Token = {
   content: string,
   type: TokenType,
+}
+
+interface IDefinition {
+  name: string;
+  position: Range;
+}
+
+export interface FunctionDefinition extends IDefinition {
+  arguments: void;
+}
+
+export interface VariableDefinition extends IDefinition {
+  content: string;
 }
