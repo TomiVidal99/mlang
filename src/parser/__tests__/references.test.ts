323c4b96c5871699a15f94050ae84284c573ac62
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-10-21 13:17:14 -0300

    Fixed forgot to rename the test

--- src/parser/__tests__/references.test.ts
+++ src/parser/__tests__/references.test.ts
@@ -1,6 +1,27 @@
 import { expect, test } from "bun:test";
 import { Parser, Tokenizer, Visitor } from "..";
 
+test("Handle references: string, number and vector references", function() {
+  const inputCode = `
+    stringReference = "STRING";
+    numberReference1 = 1000 + 1e3;
+    vectorReference = [100:1000];
+  `;
+
+  const tokenizer = new Tokenizer(inputCode);
+  const tokens = tokenizer.getAllTokens();
+  const parser = new Parser(tokens);
+  const ast = parser.makeAST();
+  const visitor = new Visitor();
+  visitor.visitProgram(ast);
+  const documentReferences = visitor.references;
+
+  const expectedReferences: string[] = ["stringReference", "numberReference1", "vectorReference"];
+
+  expect(documentReferences.map((ref) => ref.name)).toStrictEqual(expectedReferences);
+
+});
+
 test("Octave/Matlab Parser, should parse a simple assignment statement", function() {
   const inputCode = `
     stringReference = "STRING";
