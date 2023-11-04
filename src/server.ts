6daf341f62a31a549553fd1c1ce94e727baec4b1
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-11-04 09:07:57 -0300

    Refactor after adding eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -2,51 +2,64 @@ import {
   createConnection,
   TextDocuments,
   ProposedFeatures,
-  Diagnostic,
-} from "vscode-languageserver/node";
-import { TextDocument } from "vscode-languageserver-textdocument";
-import { handleOnInitialized, handleOnInitialize, handleReferences, handleCompletion, handleDefinitions } from "./handlers";
-import { ISettings } from "./data";
-import { Parser, Tokenizer, Visitor } from "./parser";
-import { getDiagnosticFromLitingMessage } from "./utils";
+  type Diagnostic,
+} from 'vscode-languageserver/node';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import {
+  handleOnInitialized,
+  handleOnInitialize,
+  handleReferences,
+  handleCompletion,
+  handleDefinitions,
+} from './handlers';
+import { type ISettings } from './data';
+import { Parser, Tokenizer, Visitor } from './parser';
+import { getDiagnosticFromLitingMessage } from './utils';
 
 // THIS IS THE TIME THAT IT WAITS BEFORE TRIGGERING A REFRESH
 // OF PARSING THE DOCUMENT WHEN THE USER IT'S TYPING
 const DEBOUNCE_DELAY_MS = 1000;
 
 export const connection = createConnection(ProposedFeatures.all);
-const documentSettings = new Map < string, Thenable<ISettings>>();
+const documentSettings = new Map<string, Thenable<ISettings>>();
 
-  export const documents = new TextDocuments<TextDocument>(TextDocument);
-    export const visitors = new Map<string, Visitor>();
-    const documentChanges: Map<string, NodeJS.Timer> = new Map();
+export const documents = new TextDocuments<TextDocument>(TextDocument);
+export const visitors = new Map<string, Visitor>();
+const documentChanges = new Map<string, NodeJS.Timer>();
 
 documents.onDidChangeContent((change) => {
-      updateDocumentData(change.document.uri, change.document.getText());
+  updateDocumentData(change.document.uri, change.document.getText());
 });
 // documents.onDidOpen((change) => {
-      // const text = change.document.getText();
-      // const uri = change.document.uri;
+// const text = change.document.getText();
+// const uri = change.document.uri;
 
-      // log(`opened '${uri}'`);
-      // });
-      // documents.onDidSave((change) => handleOnDidSave({change}));
-      connection.onInitialize((params) => handleOnInitialize({ params, connection }));
-connection.onInitialized((params) => handleOnInitialized({params, connection}));
+// log(`opened '${uri}'`);
+// });
+// documents.onDidSave((change) => handleOnDidSave({change}));
+connection.onInitialize((params) => handleOnInitialize({ params, connection }));
+connection.onInitialized((params) => {
+  handleOnInitialized({ params, connection });
+});
 // connection.onDidOpenTextDocument((params) => {});
-connection.onDefinition((params) => handleDefinitions({params, documents}));
+connection.onDefinition(
+  async (params) => await handleDefinitions({ params, documents }),
+);
 connection.onReferences((params) => {
   const uri = params.textDocument.uri;
-    const document = documents.get(uri);
-    return handleReferences(document, params.position);
+  const document = documents.get(uri);
+  if (document === undefined) return [];
+  return handleReferences(document, params.position);
 });
 // connection.onDidChangeConfiguration((change) =>);
 // connection.workspace.onDidDeleteFiles((event) => {});
-documents.onDidClose((e) => {documentSettings.delete(e.document.uri); });
+documents.onDidClose((e) => {
+  documentSettings.delete(e.document.uri);
+});
 // documents.onDidChangeContent((change) => {
-      //   updateDiagnostics(change.document.uri, change.document.getText());
-      // });
-      connection.onCompletion((params) => handleCompletion({ params: params }));
+//   updateDiagnostics(change.document.uri, change.document.getText());
+// });
+connection.onCompletion((params) => handleCompletion({ params }));
 connection.onCompletionResolve((item) => {
   // if (item.kind.valueOf() === 3 && item.command) {
   //   // IT'S A FUNCTION
@@ -59,42 +72,52 @@ connection.onCompletionResolve((item) => {
   return item;
 });
 
-    // Make the text document manager listen on the connection
-    // for open, change and close text document events
-    documents.listen(connection);
-    // Listen on the connection
-    connection.listen();
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+// Listen on the connection
+connection.listen();
 
-    /**
-      * Updates the references, definitions and diagnostics
-      * And updates that new data on the visitors map
-      */
-    function updateDocumentData(uri: string, text: string) {
+/**
+ * Updates the references, definitions and diagnostics
+ * And updates that new data on the visitors map
+ */
+function updateDocumentData(uri: string, text: string): void {
   // Clear any previously scheduled diagnostic updates for this document
   if (documentChanges.has(uri)) {
-      clearTimeout(documentChanges.get(uri));
+    clearTimeout(documentChanges.get(uri));
   }
 
   // Schedule a new diagnostic update after a delay (e.g., 500 ms)
-  documentChanges.set(uri, setTimeout(() => {
-    const document = documents.get(uri);
-    if (document) {
+  documentChanges.set(
+    uri,
+    setTimeout(() => {
+      const document = documents.get(uri);
+      if (document === null || document === undefined) return;
       const tokenizer = new Tokenizer(text);
-    const tokens = tokenizer.getAllTokens();
-    const parser = new Parser(tokens);
-    const ast = parser.makeAST();
-    const visitor = new Visitor();
-    visitor.visitProgram(ast);
+      const tokens = tokenizer.getAllTokens();
+      const parser = new Parser(tokens);
+      const ast = parser.makeAST();
+      const visitor = new Visitor();
+      visitor.visitProgram(ast);
 
-    visitors.set(uri, visitor);
+      visitors.set(uri, visitor);
 
-      const errors: Diagnostic[] = parser.getErrors().map(err => getDiagnosticFromLitingMessage(err, 'error'));
-      const warnings: Diagnostic[] = parser.getWarnings().map(warn => getDiagnosticFromLitingMessage(warn, 'warn'));
+      const errors: Diagnostic[] = parser
+        .getErrors()
+        .map((err) => getDiagnosticFromLitingMessage(err, 'error'));
+      const warnings: Diagnostic[] = parser
+        .getWarnings()
+        .map((warn) => getDiagnosticFromLitingMessage(warn, 'warn'));
 
-    connection.sendDiagnostics({
-      uri,
-      diagnostics: [...errors, ...warnings],
-      });
-    }
-  }, DEBOUNCE_DELAY_MS));
+      connection
+        .sendDiagnostics({
+          uri,
+          diagnostics: [...errors, ...warnings],
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
+    }, DEBOUNCE_DELAY_MS),
+  );
 }
